23aadf44f21f675cc7d65e649881e8d199749b2e
Wockert <25048899@student.hhs.nl>
2026-06-02 10:16:54 +0000

    aanpasing code lijnvolgtoestand.cpp

--- src/states/LijnVolgenToestand.cpp
+++ src/states/LijnVolgenToestand.cpp
@@ -1,6 +1,15 @@
 #include "LijnVolgenToestand.h"
 #include "../core/ZumoRobot.h"
 
+extern Zumo32U4LineSensors lineSensors;
+extern Zumo32U4Motors motors;
+
+unsigned int sensorValues[5];
+
+const int BASIS_SNELHEID = 200;
+const float KP = 0.2;
+const float KD = 1.5;
+
 LijnVolgenToestand::LijnVolgenToestand(ZumoRobot& robot) : RobotToestand(robot) {}
 
 void LijnVolgenToestand::enter()  {
@@ -8,6 +17,28 @@ void LijnVolgenToestand::enter()  {
 }
 
 void LijnVolgenToestand::update() {
+     int positie = lineSensors.readLine(sensorValues);
+
+    int fout = positie - 2000;
+
+    int afgeleide = fout - vorigeFout;
+
+    int correctie =
+        (KP * fout) +
+        (KD * afgeleide);
+
+    int links =
+        BASIS_SNELHEID + correctie;
+
+    int rechts =
+        BASIS_SNELHEID - correctie;
+
+    links = constrain(links, -400, 400);
+    rechts = constrain(rechts, -400, 400);
+
+    motors.setSpeeds(links, rechts);
+
+    vorigeFout = fout;
 
 }
 
