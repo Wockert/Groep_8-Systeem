37f471c95963c6573a64f344ce9077b1a346c76b
Fatima9753 <25074636@student.hhs.nl>
2026-06-08 15:13:53 +0200

    Update BlokZoekToestand.cpp

--- src/states/BlokZoekToestand.cpp
+++ src/states/BlokZoekToestand.cpp
@@ -1,16 +1,24 @@
 #include "BlokZoekToestand.h"
 #include "../core/ZumoRobot.h"
 
-BlokZoekToestand::BlokZoekToestand(ZumoRobot& robot) : RobotToestand(robot) {}
+BlokZoekToestand::BlokZoekToestand(ZumoRobot& robot)
+: RobotToestand(robot) {}
 
-void BlokZoekToestand::enter()  {
+void BlokZoekToestand::enter() {
+  Serial.println("ENTER: BlokZoekToestand");
 
+  robot.getHardware().stopMotors();
+  robot.getBlokZoeker().resetAanval();
 }
 
 void BlokZoekToestand::update() {
+  bool klaar = robot.getBlokZoeker().zoekEnDuwBlok(robot.getHardware());
 
+  if (klaar) {
+    robot.getHardware().stopMotors();
+  }
 }
 
-void BlokZoekToestand::exit()   {
-
+void BlokZoekToestand::exit() {
+  robot.getHardware().stopMotors();
 }
